4f23ed01326d44723c38f057defc658b30312896
Galymsatt <galymsatt@gmail.com>
2019-05-17 17:46:57 +0600

    tttggggggggg

--- src/main/java/kz/logistic/pl/ServletInitializer.java
+++ src/main/java/kz/logistic/pl/ServletInitializer.java
@@ -4,8 +4,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
+    private int s;
     @Override
+
+
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(PlApplication.class);
     }
